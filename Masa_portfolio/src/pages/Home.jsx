26c8779185f9da853c034e65fffcb1d941c48d0e
masahiro <m.togasaki1026@gmail.com>
2025-12-02 11:18:15 +1100

    First Commit (create project&set up TailwindCSS)

---
+++ Masa_portfolio/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export const Home = () =>{
+    return <div className="min-he-screen">
+        
+        {/* Theme Toggle -> To change light and dark mode */}
+
+        {/* Background Effects -> Animation of Wall paper */}
+
+        {/* NavBar -> Top buttons to navigate each part of portfolio */}
+
+        {/* Main Content  */}
+
+        {/* Footer  */}
+
+    </div>;
+};
